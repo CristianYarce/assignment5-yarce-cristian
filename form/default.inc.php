c770c9845d5446fd8ac6d8786db6aa10b3e816db
Cristian Yarce <cristianyarcer@gmail.com>
2021-04-17 16:37:32 -0400

    processing form added

---
+++ form/default.inc.php
@@ -0,0 +1,66 @@
+<style type="text/css">
+  *{
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+
+  body {
+    background: white;
+  }
+
+  h1 {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    color: red;
+  }
+
+  .container {
+    max-width: 900px;
+    margin: 40px auto 120px;
+    text-align: center;
+  }
+
+  header {
+    text-align: center;
+    background: #333;
+    padding: 20px 0;
+  }
+  nav ul li {
+    display: inline;
+  }
+
+  nav a {
+    text-decoration: none;
+    color: white;
+    background: red;
+    padding: 5px;
+  }
+
+  footer {
+    background: #333;
+    color: white;
+    text-align: center;
+    padding: 20px 0;
+  }
+
+</style>
+<header>
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="about.html">About Us</a></li>
+      <li><a href="services.html">Services</a></li>
+      <li><a href="contact.html">Contact Us</a></li>
+    </ul>
+  </nav>
+</header>
+
+<div class="container">
+  <h1>Sorry something went wrong with your submission</h1>
+  <p>Please try again</p>
+</div>
+
+<footer>
+  <p>This page was designed by Cristian Yarce</p>
+</footer>
